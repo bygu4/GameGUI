6a6e413d2a6a70e8512bcc23ac3ac42ae7774dad
Alexander <sacha05bugaev@gmail.com>
2024-04-05 14:51:01 +0300

    Добавьте файлы проекта.

---
+++ GameGUI/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GameGUI
+{
+    partial class Form1
+    {
+        private const int WallSize = 20;
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        private void DrawWall(
+            int x, int y, int size, System.Windows.Forms.PaintEventArgs e)
+        {
+            Rectangle wall = new Rectangle(x, y, WallSize, WallSize);
+            
+        }
+
+        private void DrawMap(
+            string fileName, System.Windows.Forms.PaintEventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                int x = 0;
+                int y = 0;
+                while (!reader.EndOfStream)
+                {
+                    char next = (char)reader.Read();
+                    if (next == '\n')
+                    {
+                        x = 0;
+                        y += WallSize
+;
+                    }
+                    else if (next != ' ')
+                    {
+                        DrawWall(x, y, WallSize, e);
+                    }
+                    x += WallSize;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ GameGUI/Program.cs
@@ -0,0 +1,11 @@
+﻿using Game;
+using Loop;
+
+var eventLoop = new EventLoop();
+var game = new Game.Game();
+eventLoop.LeftHandler += game.GoingLeft;
+eventLoop.RightHandler += game.GoingRight;
+eventLoop.DownHandler += game.GoingDown;
+eventLoop.UpHandler += game.GoingUp;
+
+eventLoop.Run();
